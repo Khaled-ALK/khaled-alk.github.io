c668b3795c10cf5a1cb07da8988c948b3119c98f
Khaled-ALK <khaledalekasirut@gmail.com>
2025-10-26 11:50:07 +0000

    Deploying to gh-pages from @ Khaled-ALK/khaled-alk.github.io@2c75fbe0bb3da9a748bb16d3c853c8ea1d2ef2d2 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Khaled-ALK/khaled-ALK",
+    "data-repo": "Khaled-ALK/khaled-alk.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
