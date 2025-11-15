7856e966ded9e9a4d1f56a10354c0e7cb1290cb6
Khaled-ALK <khaledalekasirut@gmail.com>
2025-11-15 14:39:02 +0000

    Deploying to gh-pages from @ Khaled-ALK/khaled-alk.github.io@eff7cc8b614b00d958e18b0e0c28b5895dcc310d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,14 +16,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/courses/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
