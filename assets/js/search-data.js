9d9d80009c90e36e1ce16d46a068b942751f7040
Khaled-ALK <khaledalekasirut@gmail.com>
2026-01-08 12:50:48 +0000

    Deploying to gh-pages from @ Khaled-ALK/khaled-alk.github.io@90a35a7ed209f802862503dbaae8b7bdf746b9cd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -94,7 +94,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6B%68%61%6C%65%64%61%6C%65%6B%61%73%69%72%75%74@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -102,6 +102,13 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/khaled-alekasir-b163291b7", "_blank");
         },
       },{
         id: 'social-rss',
@@ -116,13 +123,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
