4ee5748f4984893753a39c50047c59bce2a39dc4
Khaled-ALK <khaledalekasirut@gmail.com>
2026-07-27 21:27:28 +0000

    Deploying to gh-pages from @ Khaled-ALK/khaled-alk.github.io@60db9f4ff15ad4af6c3b8db2bf357e67998a3a74 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -74,11 +74,6 @@ ninja.data = [{
           description: "Notes, readings, and references for the BIMSA course Probability 3: Brownian Motion and Stochastic Calculus.",
           section: "Projects",handler: () => {
               window.location.href = "/courses/brownian-motion-stochastic-calculus/";
-            },},{id: "projects-convex-optimization-i",
-          title: 'Convex Optimization I',
-          description: "Core ideas, theory, and algorithms for convex optimization (Boyd-style).",
-          section: "Projects",handler: () => {
-              window.location.href = "/courses/convex-optimization-i/";
             },},{id: "projects-game-theory",
           title: 'Game Theory',
           description: "Notes, readings, and problem sets from my Game Theory course which I audited in Sharif University.",
